2efdcc46849ca4e3d51005a9dbb0f87d1699f75f
larry_the_cow <larry@gentoo.org>
2020-01-09 02:47:01 +0000

    esci-0.6.1

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,15 +1,16 @@
 --[[
-These code snippets are by silene and melinath on the Wesnoth forums.  I do
-not know Lua, so several features of this campaign would not have been
-possible without the helpful posts of these fine individuals!
+These code snippets are modified versions of code by silene and melinath on the
+Wesnoth forums.  I do not know Lua, so several features of this campaign would
+not have been possible without the helpful posts of these fine individuals!
 
 http://forums.wesnoth.org/viewtopic.php?f=21&t=27450
 http://forums.wesnoth.org/viewtopic.php?t=28306&p=414894
 --]]
 
---Save and load the map using a WML variable
+-- Save and load the map using a WML variable
 local helper = wesnoth.require "lua/helper.lua"
-wesnoth.register_wml_action("save_map", function(cfg)
+
+local function save_map(cfg)
   local b = cfg.border_size or 1
   local w, h = wesnoth.get_map_size()
   local t = {}
@@ -21,12 +22,15 @@ wesnoth.register_wml_action("save_map", function(cfg)
   local s = table.concat(t, '\n')
   local v = cfg.variable or helper.wml_error "save_map missing required variable= attribute."
   wesnoth.set_variable(v, string.format("border_size=%d\nusage=map\n\n%s", b, s))
-end)
+end
+wesnoth.wml_actions["save_map"] = save_map
 
-wesnoth.register_wml_action("load_map", function(cfg)
+
+local function load_map(cfg)
   local v = cfg.variable or helper.wml_error "load_map missing required variable= attribute."
   wesnoth.fire("replace_map", { map = wesnoth.get_variable(v), expand = true, shrink = true })
-end)
+end
+wesnoth.wml_actions["load_map"] = load_map
 
 
 
@@ -34,11 +38,11 @@ end)
 local function store_shroud(args)
    local team_num = args.side or error("~wml:[store_shroud] expects a side= attribute.",0)
    local storage = args.variable or error("~wml:[store_shroud] expects a variable= attribute.",0)
-   local team = wesnoth.get_side(team_num)
+   local team = wesnoth.sides[team_num]
    local current_shroud = team.__cfg.shroud_data
    wesnoth.set_variable(storage,current_shroud)
 end
-wesnoth.register_wml_action("store_shroud",store_shroud)
+wesnoth.wml_actions["store_shroud"] = store_shroud
 
 
 local helper = wesnoth.require "lua/helper.lua"
@@ -62,4 +66,4 @@ local function set_shroud(args)
       end
    end
 end
-wesnoth.register_wml_action("set_shroud", set_shroud)
+wesnoth.wml_actions["set_shroud"] = set_shroud
