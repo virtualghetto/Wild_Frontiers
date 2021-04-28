7e3ea312c488d89844f8ff30eb0e6e2cc77142e5
larry_the_cow <larry@gentoo.org>
2021-04-28 00:35:38 +0000

    wlp

--- lua/utils.lua
+++ lua/utils.lua
@@ -253,3 +253,61 @@ function wesnoth.effects.canrecruit(u, cfg)
 		add = "misc/leader-expendable.png",
 	}}, false)
 end
+
+-- https://github.com/Elvish-Hunter/Wesnoth_Lua_Pack.git
+-- modified Wesnoth_Lua_Pack/wml-tags.lua
+--[[
+[get_recruit_list]
+	[filter_side]
+		side=1
+	[/filter_side]
+	[filter]
+		side=1
+	[/filter]
+	variable=recruit_list
+[/get_recruit_list]
+--]]
+function wesnoth.wml_actions.get_recruit_list( cfg )
+	-- support function
+	-- Lua does not have the in operator as Python
+	-- in Python, "in" can be used also to check if a list contains a certain value, not only to iterate
+	local function check( t, v )
+		for i, va in ipairs( t ) do
+			if type( v ) == type( va ) and v == va then
+				return true
+			end
+		end
+		return false
+	end
+
+	local filter_side = wml.get_child( cfg, "filter_side" ) or helper.wml_error( "Missing [filter_side] in [get_recruit_list]" )
+	local filter = wml.get_child( cfg, "filter" )
+	local variable = cfg.variable or "recruit_list"
+
+	for index, side in ipairs( wesnoth.get_sides( filter_side ) ) do
+		local recruit_list = { }
+
+		for recruitable in string.gmatch( side.__cfg.recruit, '[^,]+' ) do
+			table.insert( recruit_list, recruitable )
+		end
+
+		if filter then
+			--filter.side = side.side -- to avoid collecting extra_recruit from enemies
+			for index,unit in ipairs( wesnoth.get_units( filter ) ) do
+				if unit.canrecruit and #unit.extra_recruit > 0 then
+					for extra_index, extra_recruitable in ipairs( unit.extra_recruit ) do
+						if not check( recruit_list, extra_recruitable ) then
+							table.insert( recruit_list, extra_recruitable )
+						end
+					end
+				end
+			end
+		end
+
+		wesnoth.set_variable( string.format( "%s[%d]", variable, index - 1 ), { side = side.side,
+											--team_name = side.team_name,
+											--user_team_name = side.user_team_name,
+											--name = side.side_name,
+											recruit_list = table.concat( recruit_list, "," ) } )
+	end
+end
